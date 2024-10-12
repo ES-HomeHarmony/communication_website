7837a8df88f258aea28024796d467ec76eb57eac
Sara Almeida <sarafalmeida@ua.pt>
2024-10-12 17:01:54 +0100

    hoping again

--- microsite/docusaurus.config.js
+++ microsite/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://es-homeharmony.github.io',
+  url: 'https://es-homeharmony.github.io/communication_website/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/microsite/',
